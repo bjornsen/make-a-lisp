f9fda8a1dcb4ef0be668fdbe86b4f3d7fddd5733
bjornsen <bernsen@gmail.com>
2014-11-12 09:27:39 -0500

    Made ints and floats separate types

--- prompt.c
+++ prompt.c
@@ -6,7 +6,7 @@
 #include "mpc.h"
 
 // Lisp value (lval) types
-enum { LVAL_NUM, LVAL_ERR };
+enum { LVAL_INT, LVAL_FLOAT, LVAL_ERR };
 
 // Error types
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
@@ -14,15 +14,22 @@ enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 // Defines possible return values for a lisp value
 typedef struct {
 	int type;
-	float num;
+	int i;
+	float f;
 	int err;
 } lval;
 
-
-lval lval_num(float x) {
+lval lval_int(int x) {
 	lval v;
-	v.type = LVAL_NUM;
-	v.num = x;
+	v.type = LVAL_INT;
+	v.i = x;
+	return v;
+}
+
+lval lval_float(float x) {
+	lval v;
+	v.type = LVAL_FLOAT;
+	v.f = x;
 	return v;
 }
 
@@ -36,8 +43,12 @@ lval lval_err(int x) {
 // Print an lval
 void lval_print(lval v) {
 	
-	if (v.type == LVAL_NUM) {
-		printf("%f", v.num);
+	if (v.type == LVAL_FLOAT) {		
+		printf("%f", v.f);
+	}
+	
+	if (v.type == LVAL_INT) {
+		printf("%i", v.i);
 	}
 	
 	if (v.type == LVAL_ERR) {
@@ -72,42 +83,106 @@ lval eval_op(char* op, lval x, lval y) {
 	if (x.type == LVAL_ERR) { return x; }
 	if (y.type == LVAL_ERR) { return y; } 
 	
-	if (strcmp(op, "+") == 0) { return lval_num(x.num + y.num); }
-	if (strcmp(op, "-") == 0) { return lval_num(x.num - y.num); }
-	if (strcmp(op, "*") == 0) { return lval_num(x.num * y.num); }
-	if (strcmp(op, "/") == 0) {
-		return y.num == 0
-			? lval_err(LERR_DIV_ZERO)
-			: lval_num(x.num / y.num); 
-	}
-	if (strcmp(op, "max") == 0) {
-		if (x.num > y.num) {
-			return x;
+	if ((x.type == LVAL_INT) && (y.type == LVAL_INT)) { 
+			
+		if (strcmp(op, "+") == 0) { return lval_int(x.i + y.i); }
+		if (strcmp(op, "-") == 0) { return lval_int(x.i - y.i); }
+		if (strcmp(op, "*") == 0) { return lval_int(x.i * y.i); }
+		if (strcmp(op, "/") == 0) {
+			if (y.i == 0) {
+				return lval_err(LERR_DIV_ZERO);
+			}
+			else if (x.i % y.i == 0) {
+				return lval_int(x.i / y.i);
+			}
+			else {
+				
+				return lval_float((float)x.i / (float)y.i);
+			}
 		}
-		else {
-			return y;
+		if (strcmp(op, "max") == 0) {
+			if (x.i > y.i) {
+				return x;
+			}
+			else {
+				return y;
+			}
 		}
-	}
-	if (strcmp(op, "min") == 0) {
-		if (x.num < y.num) {
-			return x;
+		if (strcmp(op, "min") == 0) {
+			if (x.i < y.i) {
+				return x;
+			}
+			else {
+				return y;
+			}
 		}
-		else {
-			return y;
+	} 
+	// If any values are float, the result will need to be a float
+	else {
+		if (x.type == LVAL_INT) {
+			x = lval_float(x.i);
+		}
+		if (y.type == LVAL_INT) {
+			y = lval_float(y.i);
+		}
+		if (strcmp(op, "+") == 0) { return lval_float(x.f + y.f); }
+		if (strcmp(op, "-") == 0) { return lval_float(x.f - y.f); }
+		if (strcmp(op, "*") == 0) { return lval_float(x.f * y.f); }
+		if (strcmp(op, "/") == 0) {
+			return y.f == 0
+				? lval_err(LERR_DIV_ZERO)
+				: lval_float(x.f / y.f); 
+		}
+		
+		if (strcmp(op, "max") == 0) {
+			if (x.f > y.f) {
+				return x;
+			}
+			else {
+				return y;
+			}
+		}
+		if (strcmp(op, "min") == 0) {
+			if (x.f < y.f) {
+				return x;
+			}
+			else {
+				return y;
+			}
 		}
 	}
 	
 	return lval_err(LERR_BAD_OP);
 }
 
+lval parse_float(mpc_ast_t* ast) {
+	errno = 0;
+	int size = ast->children_num * sizeof(char);
+	char f[size];
+	printf("Concat float is initialized to %s\n", f);
+	for (int i = 0; i < ast->children_num; i++) {
+		f[i] = *ast->children[i]->contents;
+		printf("Child is %s\n", ast->children[i]->contents);
+		printf("Float is now %s\n", f);
+	}
+	float x = strtof(f, NULL);
+	printf("Final float is %f\n", x);
+	return errno != ERANGE ? lval_float(x) : lval_err(LERR_BAD_NUM);
+}
+
 lval eval(mpc_ast_t* ast) {
 	
 	// Return numbers immediately
-	if (strstr(ast->tag, "number")) {
+	if (strstr(ast->tag, "integer")) {
 		// Error checking
 		errno = 0;
-		float x = strtof(ast->contents, NULL);
-		return errno != ERANGE ? lval_num(x) : lval_err(LERR_BAD_NUM);
+		long x = strtol(ast->contents, NULL, 10);
+		return errno != ERANGE ? lval_int(x) : lval_err(LERR_BAD_NUM);
+	}
+	
+	if (strstr(ast->tag, "float")) {
+		// Error checking
+		return parse_float(ast);
 	}
 	
 	// Operators are always the second child
@@ -118,12 +193,12 @@ lval eval(mpc_ast_t* ast) {
 	/* Account for negative numbers.  Children_num is 4 because that indicates
 	 * there is only one number in addition to the operator */
 	if (strcmp(op, "-") == 0 && ast->children_num == 4) {
-		return lval_num(-x.num);
+		return lval_float(-x.f);
 	}
 	
 	// Evalute remaining child expressions
 	int i = 3;
-	while (strstr(ast->children[i]->tag, "expr")) {
+	while (strstr(ast->children[i]->tag, "expr") || strstr(ast->children[i]->tag, "float")) {
 		x = eval_op(op, x, eval(ast->children[i]));
 		i++;
 	}
@@ -134,7 +209,7 @@ lval eval(mpc_ast_t* ast) {
 int main(int argc, char** argv) {
 	
 	/* Create parsers */
-	mpc_parser_t* Number = mpc_new("number");
+	mpc_parser_t* Integer = mpc_new("integer");
 	mpc_parser_t* Float = mpc_new("float");
 	mpc_parser_t* Operator = mpc_new("operator");
 	mpc_parser_t* Expr = mpc_new("expr");
@@ -142,8 +217,8 @@ int main(int argc, char** argv) {
 
 	mpca_lang(MPCA_LANG_DEFAULT,
   	" \
-			number	 : /-?[0-9]+/ ;	\
-			float    : <number> '.' <number> ;	\
+			integer	 : /-?[0-9]+/ ;	\
+			float    : <integer> '.' <integer> ;	\
   		operator : '+' \
   						 | '-' \
   						 | '*' \
@@ -152,18 +227,18 @@ int main(int argc, char** argv) {
   						 | \"max\" \
   						 | \"min\" ; \
   		expr     : <float> \
-  						 | <number> \
+  						 | <integer> \
   						 | '(' <operator> <expr>+ ')' ; \
   		bilisp   : /^/ <operator> <expr>+ /$/ ; \
   	",
-  	Number, Float, Operator, Expr, Bilisp);
+  	Integer, Float, Operator, Expr, Bilisp);
 	
 	puts("Bilisp 0.0.0.0.1");
 	puts("Press Ctrl+c to Exit\n");
 	
 	while (1) {
 		
-		char* input = readline("lispy> ");
+		char* input = readline("bilisp> ");
 		
 		add_history(input);
 		
@@ -171,6 +246,8 @@ int main(int argc, char** argv) {
 		mpc_result_t r;
 		if (mpc_parse("<stdin>", input, Bilisp, &r)) {
 			mpc_ast_t* ast = r.output;
+			
+			mpc_ast_print(ast);
 			
 			lval result = eval(ast);
 			lval_print(result);
@@ -187,7 +264,7 @@ int main(int argc, char** argv) {
 	}
 	
 	/* Undefine and delete parsers */
-	mpc_cleanup(5, Number, Float, Operator, Expr, Bilisp);
+	mpc_cleanup(5, Integer, Float, Operator, Expr, Bilisp);
 	
 	return 0;
 }
